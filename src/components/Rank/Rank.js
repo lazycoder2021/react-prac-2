503ef8aabc89e5eda692a3940b47cad4c21a0643
lazycoder2021 <tksingh2087@gmail.com>
2021-02-08 08:47:40 +0530

    Initial Comitt

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,38 @@
+// JavaScript source code
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Ray, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JavaScript source code
